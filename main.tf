11acbf1888968a1b3bf85835a220adda1485f546
Dharshinivelu <153889100+Dharshinivelu@users.noreply.github.com>
2024-07-09 00:19:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ data "azurerm_resource_group" "example" {
   name = var.resource_group_name
 }
 module "vnet" {
-  source                      = "module/vnet"  #../modules/vnet
-  vnet_name                        = var.vnet_name
+  source                      = ""./module/vnet""  #../modules/vnet
+  vnet_name                   = var.vnet_name
   location                    = var.location
   network_address_space       = var.network_address_space
   aks_subnet_address_prefix   = var.aks_subnet_address_prefix
   aks_subnet_address_name     = var.aks_subnet_address_name
-  vnet_environment                 = var.vnet_environment
+  vnet_environment            = var.vnet_environment
   resource_group_name         = data.azurerm_resource_group.example.name
   resource_group_location     = data.azurerm_resource_group.example.location
 }
