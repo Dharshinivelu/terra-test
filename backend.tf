633f01843d19e27b8dba371c427927fb75a2de0c
Dharshinivelu <153889100+Dharshinivelu@users.noreply.github.com>
2024-07-09 16:35:26 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name   = "terra-test"
     storage_account_name  = "terrastatetest"
     container_name        = "terrastate"
-    key                   = "terra.tfstate"
+    key                   = "terraform.tfstate"
   }
 }
