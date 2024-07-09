bf9377dd29a98583f8f006fac3aa1b910f8d4476
Dharshinivelu <153889100+Dharshinivelu@users.noreply.github.com>
2024-07-09 09:26:02 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "terra-test"
+    storage_account_name  = "terrastatetest"
+    container_name        = "terrastate"
+    key                   = "terraform.tfstate"
+  }
+}
